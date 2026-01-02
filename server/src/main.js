4c262b962ffe057a025ec4e95b85af7b015d46da
Derin Eryilmaz <mathia.is.fun@gmail.com>
2026-01-02 12:24:11 -0500

    make server listen on 0.0.0.0

--- server/src/main.js
+++ server/src/main.js
@@ -8,6 +8,6 @@ app.register(apiRoutes);
 app.setNotFoundHandler((req, res) => {
     res.code(404).send({ success: false, error: 'Not found' });
 });
-app.listen({ port: 3000 }, (err, address) => {
+app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
     console.log("Shelf finder server now running on " + address);
 });
