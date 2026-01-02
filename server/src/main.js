1320710b8359b5ecdcc306e8ef9cb2aef83f648d
Derin Eryilmaz <mathia.is.fun@gmail.com>
2026-01-02 12:44:39 -0500

    use cors on the server

--- server/src/main.js
+++ server/src/main.js
@@ -1,13 +1,18 @@
 import "./load-env.js";
 
 import Fastify from "fastify";
+import cors from '@fastify/cors';
+
 import { apiRoutes } from "./routes.js";
 
 let app = Fastify();
+
 app.register(apiRoutes);
+app.register(cors, { origin: "*" });
 app.setNotFoundHandler((req, res) => {
     res.code(404).send({ success: false, error: 'Not found' });
 });
+
 app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
     console.log("Shelf finder server now running on " + address);
 });
