354d8986884f4e6e8b7b931180dc692a31aaabf4
Derin Eryilmaz <mathia.is.fun@gmail.com>
2026-02-02 10:37:01 -0500

    remove bookmarklet stuff

--- public/index.js
+++
@@ -1,3 +0,0 @@
-let bookmark = document.getElementById("bookmark");
-let url = import.meta.resolve("/bookmark.js");
-bookmark.href = `javascript:import("${url}?" + Date.now()).catch(() => alert("Failed to run Shelf Finder!"));`;
